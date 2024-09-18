3343f89de717b6f9e7ad78bc2b76e28e33e3222e
Arron Donnelly <seriouslycrunchy@gmail.com>
2024-09-18 16:15:07 +0100

    Completed most of lession 3

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -8,17 +8,24 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+//need a reference to the other classes we want to use
+using Engine.ClassViewer;
 
-namespace RPGApp1
+namespace RPGUI
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+        private GameSession NewGameSession;
+
+        //initialise the window as part of the constructor, set up a new GameSession instance, and add it to the Data Context variable (this will be used later for xaml)
         public MainWindow()
         {
             InitializeComponent();
+            NewGameSession = new GameSession();
+            DataContext = NewGameSession;
         }
     }
 }
